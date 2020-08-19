b76fb8597fafb68799e6b8bf385e5622bed5a693
Harpo MAxx <harpomaxx@gmail.com>
2020-08-19 20:10:48 -0300

    add support for multiple characters
    Related to #0

--- app.R
+++ app.R
@@ -1,8 +1,40 @@
-#
+#https://stackoverflow.com/questions/63195691/can-i-use-observeevent-with-bsplusbs-accordion
 
 library(shiny)
 library(magick)
 library(shinyjs) 
+
+library(slickR)
+
+  
+chata_character<-  list(
+         name="Chata",
+         base_image="chata-frases-base.png"
+         )
+
+harpo_character<- list(
+        name="Harpo",
+        base_image="harpo-frases-base2.png"
+        )
+
+facha_character<- list(
+  name="Facha",
+  base_image="facha-frases-base.png"
+)
+
+rodralez_character<- list(
+  name="Rodralez",
+  base_image="rodralez-frases-base.png"
+)
+
+flequiboina_character<- list(
+  name="Prof. Flequiboina",
+  base_image="flequiboina-frases-base.png"
+)
+
+
+character_phrases_base<-list(chata_character,harpo_character,facha_character,rodralez_character,flequiboina_character)
+  
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   useShinyjs(),
@@ -11,15 +43,22 @@ ui <- fluidPage(
                       href="https://raw.githubusercontent.com/harpomaxx/harpoquotes/master/favicon.ico",
                       type="image/vnd.microsoft.icon"
             )),
-
+  
   #titlePanel(id="big-heading","Harpo Quotes Generator", windowTitle = "Harpo Quotes Generator"),
   h1(id="big-heading", "Harpo's Quotes Generator"),
   tags$style(HTML("#big-heading{background-color: pink;color: brown}")),
   h5("Now, you finally have the oportunity to make your own version of Harpo's quotes to use whenever you want. Please, use it wisely! "),
-   
+  
    # Sidebar with a slider input for number of bins 
   fluidRow(
     column(4,
+           
+           
+           #verbatimTextOutput("dbg"),
+           div(
+             p("Click on your favorite character: "),style = "font-weight: bold;color: brown"),
+           slickROutput("slick_output", width='80%',height='110%'),
+          
         div(
         textAreaInput ("text",
                   "Put your quote here",width = 300, height  = 100 ,
@@ -30,10 +69,7 @@ ui <- fluidPage(
                   "circa year",value="2020", width = 100 ),style = "color: brown"),
   
         downloadButton("down","Download")
-        
         ),
-  
-  
     column(4,imageOutput("imageQuote"))
            )
          
@@ -42,6 +78,45 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
+  
+  output$slick_output <- renderSlickR({
+    x <- slickR(c("chata-small.png",
+                  "harpo-small.png",
+                  "facha-small.png",
+                  "rodralez-small.png",
+                  "flequiboina-small.png"
+    ),
+    slideId = 'harpoquotes',
+    height = '1%',
+    width = '1%') + 
+      settings(slidesToShow=1,centerMode=FALSE,variableWidth = FALSE)
+  })
+  
+  active_slick <- shiny::reactiveValues()
+  
+  shiny::observeEvent(input$slick_output_current,{
+    
+    clicked_slide    <- input$slick_output_current$.clicked
+    relative_clicked <- input$slick_output_current$.relative_clicked
+    center_slide     <- input$slick_output_current$.center
+    total_slide      <- input$slick_output_current$.total
+    active_slide     <- input$slick_output_current$.slide
+    
+    if(!is.null(clicked_slide)){
+      active_slick$clicked_slide    <- clicked_slide
+      active_slick$center_slide     <- center_slide
+      active_slick$relative_clicked <- relative_clicked
+      active_slick$total_slide      <- total_slide
+      active_slick$active_slide     <- active_slide
+    }
+  })
+  
+#  output$dbg <- renderText({
+#    #paste("hole")
+#    l <- shiny::reactiveValuesToList(active_slick)
+#    paste(gsub('_',' ',names(l)), unlist(l),sep=' = ',collapse='\n')
+#  })
+  
  
   # Core wrapping function
   wrap.it <- function(x, len)
@@ -65,11 +140,21 @@ server <- function(input, output, session) {
   
   
   generate_quote <- function(text,year){
-    harpo_quote_base <- image_read("harpo-frases-base.png")
+    if(!is.null(active_slick$clicked_slide)){
+        char_name<-character_phrases_base[[active_slick$clicked_slide]]$name
+        char_base_image<-character_phrases_base[[active_slick$clicked_slide]]$base_image
+    }else{
+      char_name<-character_phrases_base[[1]]$name
+      char_base_image<-character_phrases_base[[1]]$base_image
+      
+    }  
+      
+    harpo_quote_base <- image_read(char_base_image)
     image_annotate(harpo_quote_base, paste0("''",wrap.labels(text,28),"''"), size = 60, color = "black",
                    degrees = 0, location = "+40+100", style="Italic", weight = 700) %>% 
-      image_annotate(input$year, size = 33, color = "black",
-                     degrees = 0, location = "+520+305") %>% 
+      image_annotate(paste0("(",char_name,", circa ", input$year,")"), 
+                     size = 40, color = "black",
+                     degrees = 0, location = "+180+355") %>% 
       image_annotate("Harpo's Quotes (R)", location = "+820+500", size=10)
   }
    
